cd6a65372062fbc2a4980ee9c7cb7b9fbd3bde91
lichen404 <f360family@gmail.com>
2020-10-06 10:43:46 +0800

    deploy

---
+++ precache-manifest.157a2caf440715370fd7de58922a68b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d913a23a9d7b2ff6d99f01cdfff87343",
+    "url": "/daily-cost-react-website/index.html"
+  },
+  {
+    "revision": "171a924f2864e61bce9c",
+    "url": "/daily-cost-react-website/static/css/main.6f1871c9.chunk.css"
+  },
+  {
+    "revision": "417af5815e2aff643633",
+    "url": "/daily-cost-react-website/static/js/2.758c869a.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/daily-cost-react-website/static/js/2.758c869a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "171a924f2864e61bce9c",
+    "url": "/daily-cost-react-website/static/js/main.48283ae1.chunk.js"
+  },
+  {
+    "revision": "fab90c2230fd49252637",
+    "url": "/daily-cost-react-website/static/js/runtime-main.04d19757.js"
+  }
+]);
