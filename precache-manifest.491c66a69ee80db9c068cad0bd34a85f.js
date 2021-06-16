e3407873fccee1447ed1e2ef1946863a0e4b5576
lichen404 <lichen@genew.com>
2021-06-16 12:29:50 +0800

    deploy

---
+++ precache-manifest.491c66a69ee80db9c068cad0bd34a85f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e638e0cc447e730d9ea68512128ab052",
+    "url": "/daily-cost-react-website/index.html"
+  },
+  {
+    "revision": "3d3604d11e846d9ae66d",
+    "url": "/daily-cost-react-website/static/css/main.94a15ded.chunk.css"
+  },
+  {
+    "revision": "417af5815e2aff643633",
+    "url": "/daily-cost-react-website/static/js/2.758c869a.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/daily-cost-react-website/static/js/2.758c869a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d3604d11e846d9ae66d",
+    "url": "/daily-cost-react-website/static/js/main.1d2bdcc6.chunk.js"
+  },
+  {
+    "revision": "fab90c2230fd49252637",
+    "url": "/daily-cost-react-website/static/js/runtime-main.04d19757.js"
+  }
+]);
